812109cbb00d89921f875096f9152ddeb57581af
Grivel-l <legrivel@student.le-101.fr>
2019-10-28 18:16:35 +0100

    Code clean

--- main.c
+++ main.c
@@ -39,10 +39,29 @@ typedef struct {
   uint32_t  ptLoad;
 }   PE64_OptHdr;
 
-int     main(int argc, char **argv) {
+typedef struct {
+  size_t  size;
+  void    *start;
+}   file;
+
+int   getStart(const char *filename, file *file) {
+  int         fd;
+  struct stat stats;
+
+  if ((fd = open(filename, O_RDONLY)) == -1)
+    return (-1);
+  if (stat(filename, &stats) == -1)
+    return (-1);
+  if ((file->start = mmap(NULL, stats.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+    return (-1);
+  file->size = stats.st_size;
+  close(fd);
+  return (0);
+}
+
+int     main(int argc, const char **argv) {
   int           fd;
-  struct stat   stats;
-  void          *start;
+  file          file;
   PE64_Ehdr     *header;
   PE64_OptHdr   *optHeader;
   PE64_Shdr     *sectionHeader;
@@ -51,26 +70,18 @@ int     main(int argc, char **argv) {
     dprintf(2, "Not right number or arguments\n");
     return (1);
   }
-  fd = open(argv[1], O_RDONLY);
-  if (stat(argv[1], &stats) == -1)
+  if (getStart(argv[1], &file) == -1)
     return (1);
-  if ((start = mmap(NULL, stats.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
-    return (1);
-  /* dprintf(1, "Header: %i, %i, %i, %i,%i,%i,%i\n", header->machine, header->shnum, header->timestamp, header->symTbl, header->symNum, header->optHeaderSize, header->flags); */
-  header = start;
-  uint32_t yo;
-  yo = *((uint32_t *)(((void *)header) + 0x3c));
-  header = ((void *)header) + yo + 4;
-  /* dprintf(1, "Header: %i, %i, %i, %i,%i,%i,%i\n", header->machine, header->shnum, header->timestamp, header->symTbl, header->symNum, header->optHeaderSize, header->flags); */
+  header = file.start + *((uint32_t *)(file.start + 0x3c)) + 4;
+  dprintf(1, "Header: %i, %i, %i, %i,%i,%i,%i\n", header->machine, header->shnum, header->timestamp, header->symTbl, header->symNum, header->optHeaderSize, header->flags);
   sectionHeader = ((void *)header) + sizeof(PE64_Ehdr) + header->optHeaderSize;
   optHeader = ((void *)header) + sizeof(PE64_Ehdr);
   dprintf(1, "Entry point: %p\n", NULL + optHeader->entryPoint);
   write(1, &(sectionHeader->name), 8);
-  close(fd);
   if ((fd = open("./packed.exe", O_CREAT | O_TRUNC | O_WRONLY, S_IRWXU | S_IRGRP | S_IXGRP | S_IXOTH | S_IROTH)) == -1)
     return (1);
-  write(fd, start, stats.st_size);
+  write(fd, file.start, file.size);
   close(fd);
-  munmap(start, stats.st_size);
+  munmap(file.start, file.size);
   return (0);
 }
